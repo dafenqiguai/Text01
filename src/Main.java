fd7f23342556bd6f96805fa3aa5e2745a3eb2c3e
dafenqi <1197155110@qq.com>
2019-04-15 17:39:36 +0800

    修改第二次

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wor有几天假58555555555ld!");
+        System.out.println("Hello Wor有几天Greg5555ld!");
     }
 }
